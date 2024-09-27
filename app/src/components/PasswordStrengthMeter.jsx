dda62485502bf17f5f3d3776481cc436b46c7ab9
tcodes317 <tcodes317@gmail.com>
2024-09-27 14:53:30 +0100

    9/27/2024

---
+++ app/src/components/PasswordStrengthMeter.jsx
@@ -0,0 +1,63 @@
+const PasswordCriteria = ({password}) => {
+
+    const criteria = [
+        { label: "At least 6 characters", met: password.length >= 6 },
+        { label: "Contains uppercase letter", met: /[A-Z]/.test(password) },
+        { label: "Contains lowercase letter", met: /[a-z]/.test(password) },
+        { label: "Contains special character", met: /[^A-Za-z0-9]/.test(password) },
+        { label: "Contains special character", met: /[^A-Za-z0-9]/.test(password)}
+    ];
+
+    return(
+        <div className="mt-2 space-y-1">
+            {
+                criteria.map((item, index) => {
+                    <div key={item.label} className="flex items-center text-xs">
+                        {item.met ? (
+                            <Check className="size-4 text-green-500 mr-2" />
+                            ) : (
+                            <X className="size-4 text-gray-500 mr-2" />
+                        )}
+                        <span className={item.met ? "text-green-500" : "text-gray-400"}>{item.label}</span>
+                    </div>
+                })
+            }
+        </div>
+    )
+}
+
+function PasswordStrengthMeter({password}){
+    const getStrengh = (pass) => {
+        let strength = 0;
+
+        if(pass.length) >= 6) strength++;
+        if(pass.match(/[a-z]/) && pass.match(/[A-Z]/)) strength++;
+        if(pass.match(/\d/)) strength++;
+        if(pass.match(/[^a-zA-Z\d]/)) strength++;
+        
+        return strength;
+    };
+
+    const strength = getStrengh(password);
+    
+    const getStrenghText = (strength) => {
+        if(strength === 0) return "Very Weak";
+        if(strength === 1) return "Weak";
+        if(strength === 2) return "Fair";
+        if(strength === 3) return "Good";
+
+        return "Strong";
+    };
+
+    return(
+        <>
+            <div className="mt-2 ">
+                <div className="flex justify-center items-center mb-1">
+                    <span className="text-xs text-gray-400">PasswordStrengthMeter</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PasswordStrengthMeter;
--- app/src/pages/SignUpPage.jsx
+++ app/src/pages/SignUpPage.jsx
@@ -47,6 +47,13 @@ function SignUpPage(){
                             value={email}
                             onChange={(e) => setPassword(e.target.value)} 
                         />
+                        <motion.button className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+                         whileHover={{scale: 1.02}}
+                         whileTap={{scale: 0.98}}
+                         type="submit"
+                        >
+                            Sign up
+                        </motion.button>
                     </form>
                 </div>
             </motion.div>
